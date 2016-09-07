0c869b2e004c72c32e4a603af115907f11f2e67e
harshs10 <code.harsh@gmail.com>
2016-09-07 17:05:53 -0400

    Final

---
+++ Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WindowsFormsApplication6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (var series in chart1.Series)
+            {
+                series.Points.Clear();
+            }
+
+            for (int i = 0; i < 30; i++)
+            {
+                float a = Program.l1.ElementAt(i).getb();
+                chart1.Series["method1"].Points.AddXY(i + 1, a);
+                float b = Program.l2.ElementAt(i).getb();
+                chart1.Series["method2"].Points.AddXY(i + 1, b);
+            }
+
+            chart1.Series["method1"].ChartType = SeriesChartType.FastLine;
+            chart1.Series["method1"].Color = Color.Red;
+
+            chart1.Series["method2"].ChartType = SeriesChartType.FastLine;
+            chart1.Series["method2"].Color = Color.Blue;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            foreach (var series in chart1.Series)
+            {
+                series.Points.Clear();
+            }
+            for (int i = 0; i < 30; i++)
+            {
+                float a = Program.l1.ElementAt(i).getc();
+                chart1.Series["method1"].Points.AddXY(i + 1, a);
+                float b = Program.l2.ElementAt(i).getc();
+                chart1.Series["method2"].Points.AddXY(i+1, b);
+            }
+
+            chart1.Series["method1"].ChartType = SeriesChartType.FastLine;
+            chart1.Series["method1"].Color = Color.Red;
+
+            chart1.Series["method2"].ChartType = SeriesChartType.FastLine;
+            chart1.Series["method2"].Color = Color.Blue;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            foreach (var series in chart1.Series)
+            {
+                series.Points.Clear();
+            }
+            for (int i = 0; i < 30; i++)
+            {
+                float a = Program.l1.ElementAt(i).getd();
+                chart1.Series["method1"].Points.AddXY(i + 1, a);
+                float b = Program.l2.ElementAt(i).getd();
+                chart1.Series["method2"].Points.AddXY(i + 1, b);
+            }
+
+            chart1.Series["method1"].ChartType = SeriesChartType.FastLine;
+            chart1.Series["method1"].Color = Color.Red;
+
+            chart1.Series["method2"].ChartType = SeriesChartType.FastLine;
+            chart1.Series["method2"].Color = Color.Blue;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            foreach (var series in chart1.Series)
+            {
+                series.Points.Clear();
+            }
+            for (int i = 0; i < 30; i++)
+            {
+                float a = Program.l1.ElementAt(i).gete();
+                chart1.Series["method1"].Points.AddXY(i + 1, a);
+                float b = Program.l2.ElementAt(i).gete();
+                chart1.Series["method2"].Points.AddXY(i + 1, b);
+            }
+
+            chart1.Series["method1"].ChartType = SeriesChartType.FastLine;
+            chart1.Series["method1"].Color = Color.Red;
+
+            chart1.Series["method2"].ChartType = SeriesChartType.FastLine;
+            chart1.Series["method2"].Color = Color.Blue;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            foreach (var series in chart1.Series)
+            {
+                series.Points.Clear();
+            }
+            for (int i = 0; i < 30; i++)
+            {
+                float a = Program.l1.ElementAt(i).getf();
+                chart1.Series["method1"].Points.AddXY(i + 1, a);
+                float b = Program.l2.ElementAt(i).getf();
+                chart1.Series["method2"].Points.AddXY(i + 1, b);
+            }
+
+            chart1.Series["method1"].ChartType = SeriesChartType.FastLine;
+            chart1.Series["method1"].Color = Color.Red;
+
+            chart1.Series["method2"].ChartType = SeriesChartType.FastLine;
+            chart1.Series["method2"].Color = Color.Blue;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            foreach (var series in chart1.Series)
+            {
+                series.Points.Clear();
+            }
+            for (int i = 0; i < 30; i++)
+            {
+                float a = Program.l1.ElementAt(i).getg();
+                chart1.Series["method1"].Points.AddXY(i + 1, a);
+                float b = Program.l2.ElementAt(i).getg();
+                chart1.Series["method2"].Points.AddXY(i + 1, b);
+            }
+
+            chart1.Series["method1"].ChartType = SeriesChartType.FastLine;
+            chart1.Series["method1"].Color = Color.Red;
+
+            chart1.Series["method2"].ChartType = SeriesChartType.FastLine;
+            chart1.Series["method2"].Color = Color.Blue;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            foreach (var series in chart1.Series)
+            {
+                series.Points.Clear();
+            }
+            for (int i = 0; i < 30; i++)
+            {
+                float a = Program.l1.ElementAt(i).geth();
+                chart1.Series["method1"].Points.AddXY(i + 1, a);
+                float b = Program.l2.ElementAt(i).geth();
+                chart1.Series["method2"].Points.AddXY(i + 1, b);
+            }
+
+            chart1.Series["method1"].ChartType = SeriesChartType.FastLine;
+            chart1.Series["method1"].Color = Color.Red;
+
+            chart1.Series["method2"].ChartType = SeriesChartType.FastLine;
+            chart1.Series["method2"].Color = Color.Blue;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            foreach (var series in chart1.Series)
+            {
+                series.Points.Clear();
+            }
+            for (int i = 0; i < 30; i++)
+            {
+                float a = Program.l1.ElementAt(i).geti();
+                chart1.Series["method1"].Points.AddXY(i+1, a);
+                float b = Program.l2.ElementAt(i).geti();
+                chart1.Series["method2"].Points.AddXY(i+1, b);
+            }
+
+            chart1.Series["method1"].ChartType = SeriesChartType.FastLine;
+            chart1.Series["method1"].Color = Color.Red;
+
+            chart1.Series["method2"].ChartType = SeriesChartType.FastLine;
+            chart1.Series["method2"].Color = Color.Blue;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            foreach (var series in chart1.Series)
+            {
+                series.Points.Clear();
+            }
+            for (int i = 0; i < 30; i++)
+            {
+                float a = Program.l1.ElementAt(i).getj();
+                chart1.Series["method1"].Points.AddXY(i + 1, a);
+                float b = Program.l2.ElementAt(i).getj();
+                chart1.Series["method2"].Points.AddXY(i + 1, b);
+            }
+
+            chart1.Series["method1"].ChartType = SeriesChartType.FastLine;
+            chart1.Series["method1"].Color = Color.Red;
+
+            chart1.Series["method2"].ChartType = SeriesChartType.FastLine;
+            chart1.Series["method2"].Color = Color.Blue;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            foreach (var series in chart1.Series)
+            {
+                series.Points.Clear();
+            }
+            for (int i = 0; i < 30; i++)
+            {
+                float a = Program.l1.ElementAt(i).getk();
+                chart1.Series["method1"].Points.AddXY(i + 1, a);
+                float b = Program.l2.ElementAt(i).getk();
+                chart1.Series["method2"].Points.AddXY(i + 1, b);
+            }
+
+            chart1.Series["method1"].ChartType = SeriesChartType.FastLine;
+            chart1.Series["method1"].Color = Color.Red;
+
+            chart1.Series["method2"].ChartType = SeriesChartType.FastLine;
+            chart1.Series["method2"].Color = Color.Blue;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            foreach (var series in chart1.Series)
+            {
+                series.Points.Clear();
+            }
+            for (int i = 0; i < 30; i++)
+            {
+                float a = Program.l1.ElementAt(i).getl();
+                chart1.Series["method1"].Points.AddXY(i + 1, a);
+                float b = Program.l2.ElementAt(i).getl();
+                chart1.Series["method2"].Points.AddXY(i + 1, b);
+            }
+
+            chart1.Series["method1"].ChartType = SeriesChartType.FastLine;
+            chart1.Series["method1"].Color = Color.Red;
+
+            chart1.Series["method2"].ChartType = SeriesChartType.FastLine;
+            chart1.Series["method2"].Color = Color.Blue;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            foreach (var series in chart1.Series)
+            {
+                series.Points.Clear();
+            }
+            for (int i = 0; i < 30; i++)
+            {
+                float a = Program.l1.ElementAt(i).getm();
+                chart1.Series["method1"].Points.AddXY(i + 1, a);
+                float b = Program.l2.ElementAt(i).getm();
+                chart1.Series["method2"].Points.AddXY(i + 1, b);
+            }
+
+            chart1.Series["method1"].ChartType = SeriesChartType.FastLine;
+            chart1.Series["method1"].Color = Color.Red;
+
+            chart1.Series["method2"].ChartType = SeriesChartType.FastLine;
+            chart1.Series["method2"].Color = Color.Blue;
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication6
+{
+    static class Program
+    {
+        //class info
+        public class info
+        {
+            float a, b, c, d, e, f, g, h, i, j, k, l, m;
+
+            public info()
+            {
+            }
+            public info(List<float> listA)
+            {
+                this.a = listA.ElementAt(0);
+                this.b = listA.ElementAt(1);
+                this.c = listA.ElementAt(2);
+                this.d = listA.ElementAt(3);
+                this.e = listA.ElementAt(4);
+                this.f = listA.ElementAt(5);
+                this.g = listA.ElementAt(6);
+                this.h = listA.ElementAt(7);
+                this.i = listA.ElementAt(8);
+                this.j = listA.ElementAt(9);
+                this.k = listA.ElementAt(10);
+                this.l = listA.ElementAt(11);
+                this.m = listA.ElementAt(12);
+            }
+            public float getb()
+            {
+                return b;
+            }
+            public float getc()
+            {
+                return c;
+            }
+            public float getd()
+            {
+                return d;
+            }
+            public float gete()
+            {
+                return e;
+            }
+            public float getf()
+            {
+                return f;
+            }
+            public float getg()
+            {
+                return g;
+            }
+            public float geth()
+            {
+                return h;
+            }
+            public float geti()
+            {
+                return i;
+            }
+            public float getj()
+            {
+                return j;
+            }
+            public float getk()
+            {
+                return k;
+            }
+            public float getl()
+            {
+                return l;
+            }
+            public float getm()
+            {
+                return m;
+            }
+        }
+        public static List<info> l1 = new List<info>();
+        public static List<info> l2 = new List<info>();
+        public static info obj = new info();
+        public static info obj2 = new info();
+        [STAThread]
+        //Main function
+        public static void Main(string[] args)
+        {
+            //Change file path
+            var reader = new StreamReader(File.OpenRead(@""));
+
+            reader.ReadLine();
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+                int i = 0;
+                List<float> listA = new List<float>();
+                while (i < 13)
+                {                    
+                    float x = float.Parse(values[i]);
+                    listA.Add(x);
+                    i++;
+                }
+                obj = new info(listA);
+                l1.Add(obj);
+
+                line = reader.ReadLine();
+                values = line.Split(',');
+                i = 0;
+                List<float> listB = new List<float>();
+                while (i < 13)
+                {
+                    float x = float.Parse(values[i]);
+                    listB.Add(x);
+                    i++;
+                }
+                info obj2 = new info(listB);
+                l2.Add(obj2);
+            }
+            
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
